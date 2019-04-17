717416317186b37099c106883a51bbfdc8d140c5
Pratiibh <pratiibhbassi@gmail.com>
2019-04-17 10:10:23 -0700

    dummy data has been removed, second commit complete

--- server.js
+++ server.js
@@ -80,23 +80,6 @@ function searchWeatherData() {
 }
 
 
-/*
-```
-[
-  {
-    "forecast": "Partly cloudy until afternoon.",
-    "time": "Mon Jan 01 2001"
-  },
-  {
-    "forecast": "Mostly cloudy in the morning.",
-    "time": "Tue Jan 02 2001"
-  },
-  ...
-]
-```
-*/
-
-
 
 // server start
 app.listen(PORT, () => {
