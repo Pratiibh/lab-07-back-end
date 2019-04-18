47cae1188b4dc7e1a97f57acf5826386266537d4
Harris <harris.richardson87@gmail.com>
2019-04-18 13:39:26 -0700

    SQL

--- server.js
+++ server.js
@@ -11,28 +11,32 @@ const superagent = require('superagent');
 // server definition
 const app = express();
 app.use(cors());
-//const pq = require('pg')//install npm
+const pq = require('pg')//install npm
 
-//postgres client setup//*
-//const client = new pg.Client( process.env.DATABASE_URL);
-//client.connect();
-//client.on('error', error => console.error(error));
+const client = new pg.Client( process.env.DATABASE_URL);
+client.connect();
+client.on('error', error => console.error(error));
 // what the server does
 //the route
-//request = data from query. example, from a front end query
+
 //can test in localhost:3000/location to verify
+
 app.get('/location', (request, response) => {
   const search_query = request.query.data
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.GEOCODE_API_KEY}`
   const data = superagent.get(url).then(result => {
+    client.query();
+
     const resultBody = result.body;
     const formatted_query = resultBody.results[0].formatted_address;
     const latitude = resultBody.results[0].geometry.location.lat;
     const longitude = resultBody.results[0].geometry.location.lng;
     const responseObject = { search_query, formatted_query, latitude, longitude};
     response.send(responseObject);
+
+
   })
-.catch(console.log)
+    .catch(console.log)
 })
 
 app.get('/weather', (request, response) =>{
@@ -41,7 +45,7 @@ app.get('/weather', (request, response) =>{
   const dailyWeather = weatherJSON.daily;
   const dailyWeatherData = dailyWeather.data;
   let theDaily = dailyWeatherData.map(dayObj => {
-    return new DailyWeather(dayObj); 
+    return new DailyWeather(dayObj);
   })
   response.send(theDaily);
 })
@@ -62,4 +66,4 @@ function DailyWeather(rawDayObj){
 //server start
 app.listen(PORT, ()=> {
   console.log(`app is up on PORT ${PORT}`)
-})
+});
